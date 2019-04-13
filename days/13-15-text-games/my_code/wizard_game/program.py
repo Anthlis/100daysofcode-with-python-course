4573eff9e4a32c4186839a55a3bd9d6df779c011
Anthony Lister <anthony.lister@gmail.com>
2019-04-13 21:29:50 +1200

    Revisited App7 TalkPython Jumpstarter course notes on classes. Updates...

--- days/13-15-text-games/my_code/wizard_game/program.py
+++ days/13-15-text-games/my_code/wizard_game/program.py
@@ -2,7 +2,7 @@
 import random
 import time
 
-from actors import Wizard, Creature
+from actors import Wizard, Creature, SmallAnimal, Dragon
 
 
 def main():
@@ -20,11 +20,11 @@ def print_header():
 def game_loop():
 
     creatures = [
-        Creature('Toad', 1),
+        SmallAnimal('Toad', 1),
         Creature('Tiger', 12),
-        Creature('Bat', 3),
-        Creature('Dragon', 50),
-        Creature('Evil Wizard', 1000),
+        SmallAnimal('Bat', 3),
+        Dragon('Dragon', 50, 75, True),
+        Wizard('Evil Wizard', 1000),
     ]
 
     # print(creatures)
@@ -56,6 +56,10 @@ def game_loop():
             print('The Wizard has become unsure of its power and flees....')
         else:
             print('OK... exiting game')
+            break
+
+        if not creatures:
+            print(f'You have defeated all the creatures, well done!')
             break
 
         print()
